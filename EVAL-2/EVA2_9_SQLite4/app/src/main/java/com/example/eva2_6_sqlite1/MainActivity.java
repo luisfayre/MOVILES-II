4db0cc7d1a5b3f6e61d41e525816e406cf6aaa9f
Luisfayre <luisfayre@hotmail.com>
2020-04-03 22:26:54 -0700

    EVA2_9_SQLite4

---
+++ EVAL-2/EVA2_9_SQLite4/app/src/main/java/com/example/eva2_6_sqlite1/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.eva2_6_sqlite1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.os.Bundle;
+import android.os.Environment;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+    SQLiteDatabase db;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView txtMsg = (TextView) findViewById(R.id.txtMsg);
+
+        try {
+            String SDCardPath = Environment.getExternalStorageDirectory().getPath() + "/Users";
+            db = this.openOrCreateDatabase(SDCardPath,MODE_PRIVATE,null);
+            //CREATE TABLED AND ADD A USER
+            createTable("usuario","usuario@usuariomail.com");
+
+            db.close();
+            txtMsg.append(db.getPath());
+            txtMsg.append("\nDB created");
+        } catch (SQLiteException e) {
+            txtMsg.append("\nERROR " + e.getMessage());
+        }
+    }
+
+
+    private void createTable(String uName, String uEmail){
+        String msg = "USER SUCEFULLY CREATED";
+        db.beginTransaction();
+        try {
+            //perform your database operations here ...
+            db.execSQL("CREATE TABLE IF NOT EXISTS PEOPLE ("
+                    + " recID integer PRIMARY KEY autoincrement, "
+                    + " name text, "
+                    + " email text ); " );
+            db.execSQL( "insert into PEOPLE(name, email) values ( '"+ uName +"', '"+uEmail+"');" );
+            db.setTransactionSuccessful(); //commit your changes
+        }
+        catch (SQLiteException e) {
+            //report problem
+            msg = "ERROR: " + e.getMessage();
+        }
+        finally {
+            db.endTransaction();
+            Toast.makeText(this, msg,Toast.LENGTH_LONG).show();
+        }
+
+    }
+}
+
+
+
