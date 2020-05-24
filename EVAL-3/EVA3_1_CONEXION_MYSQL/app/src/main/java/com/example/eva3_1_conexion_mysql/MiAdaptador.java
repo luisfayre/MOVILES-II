e4b08d85d65a9e4f18485e9cb45c4933d1c46f61
Luisfayre <luisfayre@hotmail.com>
2020-05-23 20:16:30 -0600

    EVA3_1_CONEXION_MYSQL

---
+++ EVAL-3/EVA3_1_CONEXION_MYSQL/app/src/main/java/com/example/eva3_1_conexion_mysql/MiAdaptador.java
@@ -0,0 +1,52 @@
+package com.example.eva3_1_conexion_mysql;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class MiAdaptador extends ArrayAdapter<JSONObject> {
+    Context context;
+    int resource;
+    List<JSONObject> objects;
+
+    public MiAdaptador(@NonNull Context context, int resource, @NonNull List<JSONObject> objects) {
+        super(context, resource, objects);
+        this.context = context;
+        this.resource = resource;
+        this.objects = objects;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if(convertView == null){
+            LayoutInflater layoutInflater = ((Activity)context).getLayoutInflater();
+            convertView = layoutInflater.inflate(resource, parent, false);
+
+        }
+        TextView txtVeFirstN ,txtVwLastN;
+        txtVeFirstN = convertView.findViewById(R.id.txtVwFirstN);
+        txtVwLastN = convertView.findViewById(R.id.txtVwLastN);
+
+        try {
+            txtVeFirstN.setText(objects.get(position).getString("first_name"));
+            txtVwLastN.setText(objects.get(position).getString("last_name"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return convertView;
+    }
+}
